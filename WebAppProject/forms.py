42be21ba388bb6dda1e96dba1e197de57e0787d7
unknown <>
2017-03-10 17:30:07 +0000

    Fixed UserProfile forms and authentication

--- WebAppProject/forms.py
+++ WebAppProject/forms.py
@@ -28,45 +28,13 @@ class CommentForm(forms.ModelForm):
             model = Comment
             fields = ('title', 'content')
 
-
-#class UserForm(forms.ModelForm):
-    # name = forms.CharField(label='Full Name')
-    # password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
-    # password2 = forms.CharField(label='Password Confirmation', widget=forms.PasswordInput(),
-    #                             help_text="Please Enter the same password as above")
-    # weight = forms.IntegerField(label='Weight')
-    # height = forms.IntegerField(label='Height')
-
-   # class Meta:
-    #    model = User
-     #   fields = ['name','username','email','password','weight','height']
-
-
 class UserForm(forms.ModelForm):
-      class Meta:
-          model = User
-          fields = ['first_name', 'last_name', 'email']
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
 
-          def clean_password2(self):
-              password1 = self.cleaned_data.get("password1")
-              password2 = self.cleaned_data.get("password2")
-              if password1 and password2 and password1 != password2:
-                  raise forms.ValidationError(
-                      self.error_messages['password_mismatch'],
-                      code='password_mismatch',
-                  )
-                  self.instance.username = self.cleaned_data.get('username')
-                  password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
-                  return password2
-
-          def save(self, commit=True):
-              user = super.save(commit=False)
-              user.set_password(self.cleaned_data["password1"])
-              if commit:
-                  user.save()
-              return user
-
-class ProfileForm(forms.ModelForm):
+class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('height','weight')
+        fields = ('height','weight', 'picture')

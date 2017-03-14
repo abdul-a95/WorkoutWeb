1f823bf5ead6abfb3061717b78b47b089a440aa0
Millie Norton <2192245N@student.gla.ac.uk>
2017-03-14 18:41:02 +0000

    Contact Us & spice

--- WebAppProject/views.py
+++ WebAppProject/views.py
@@ -7,6 +7,11 @@ from WebAppProject.forms import UserForm,UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from WebAppProject.forms import ContactForm
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.loader import get_template
 
 def index(request):
     category_list = Category.objects.order_by()[:6]
@@ -207,3 +212,39 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
+
+def contact(request):
+    form_class = ContactForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+                , '')
+            contact_email = request.POST.get(
+                'contact_email'
+                , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the
+            # contact information
+            template =\
+                get_template('contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Workout Web" + '',
+                ['milsnorton@hotmail.com'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            return redirect('contact')
+    return render(request, 'contact.html',{'form': form_class})
